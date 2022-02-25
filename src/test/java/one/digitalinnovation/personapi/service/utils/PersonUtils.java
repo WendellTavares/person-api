88903dcc4569891a01667d63a8f8d06e70b5bd88
Wendell Tavares <wendell.tav@gmail.com>
2022-02-24 21:38:24 -0300

    first commit

---
+++ src/test/java/one/digitalinnovation/personapi/service/utils/PersonUtils.java
@@ -0,0 +1,37 @@
+package one.digitalinnovation.personapi.service.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Wendell";
+    private static final String LAST_NAME = "Tav";
+    private static final String CPF_NUMBER = "261.896.600-70";
+    private static final long PERSON_ID = 1l;
+    private static final LocalDate BIRTH_DATE = LocalDate.of(1990,01,01);
+
+    public static PersonDTO createFakeDTO(){
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("01-01-1990")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity(){
+        return Person.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+
+}
